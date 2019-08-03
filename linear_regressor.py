1a74e84ce1c4dcc794f21428326b40ffa3e48148
GUNDA ROHITH <rohithgunda6@gmail.com>
2019-08-03 22:36:11 +0530

    Update linear_regressor.py

--- linear_regressor.py
+++ linear_regressor.py
@@ -1,15 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug  3 20:35:33 2019
-
-@author: rogunda
-"""
 import pandas as pd  
 import numpy as np  
 import matplotlib.pyplot as plt  
-import seaborn as seabornInstance 
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 
-dataset=pd.read_csv("C:\Users\rogunda\Desktop\machine learning\)
+dataset=pd.read_csv("Weather_ww2.csv")
+print(dataset.describe())
+X = dataset['MinTemp'].values.reshape(-1,1)
+y = dataset['MaxTemp'].values.reshape(-1,1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+
+regressor = LinearRegression()  
+regressor.fit(X_train, y_train)
+
+
+print(regressor.intercept_)
+print(regressor.coef_)
+
+y_pred = regressor.predict(X_test)
+
+df = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()})
+
+print(df)
+
+plt.scatter(X_test, y_test,  color='gray')
+plt.plot(X_test, y_pred, color='red', linewidth=2)
+plt.show()
+
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
