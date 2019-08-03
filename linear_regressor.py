a70b38c268038da9271dac0a73d68eed92f9ebf6
GUNDA ROHITH <rohithgunda6@gmail.com>
2019-08-03 22:32:49 +0530

    Add files via upload

---
+++ linear_regressor.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  3 20:35:33 2019
+
+@author: rogunda
+"""
+import pandas as pd  
+import numpy as np  
+import matplotlib.pyplot as plt  
+import seaborn as seabornInstance 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+dataset=pd.read_csv("C:\Users\rogunda\Desktop\machine learning\)
